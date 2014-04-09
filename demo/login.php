bc9826ca969b469b4b0b21538cac53fcbb7115bb
MatthewJohnson3154 <mjohnson3154@gmail.com>
2014-04-09 18:46:33 -0400

    Updated Logged In and Index pages
    Also added login.php, beginning to link stuff together with the DB.

---
+++ demo/login.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>GOOD, Inc.</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+	<!--script src="js/less-1.3.3.min.js"></script-->
+	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+	
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
+
+  <!-- Fav and touch icons 
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
+  <link rel="shortcut icon" href="img/favicon.png">-->
+  
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+</head>
+<body class="home">
+	<br><br>
+	<div class="logo"></div>
+		<div class="container">
+			<div class="row clearfix">
+				<div class="col-md-12 column">
+					<div class="jumbotron">
+				        <form class="form-horizontal" method="POST" action="index.php">
+  							<div class="control-group">
+    							<p class='login'>Email</p>
+    							<div class="controls">
+      								<input type="email" name="email" id="email" placeholder="email@domain.com">
+    							</div>
+  							</div>
+  							<div class="control-group">
+    							<p class='login'>Password</p>
+    							<div class="controls">
+      								<input type="password" name="password" id="password" placeholder="Password">
+    							</div>
+  							</div>
+  							<div class="control-group">
+    							<div class="controls">
+      								<p class='login'>
+        								<input type="checkbox" name="remember" id="remember"> Remember me
+      								</p>
+      								<button type="submit" name="submit" id="submit" class="btn btn-darkgood">Log In »</button>
+      								<a class="btn btn-darkgood" href="index.html">Return Home »</a>
+    							</div>
+  							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+</body>
+</html>
